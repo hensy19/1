9af5e072ca0be5919fa16a8db4a96cb930da5c8d
hensy <hensy@HENSY>
2025-06-30 23:18:11 +0530

    Add project files.

---
+++ 1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    internal class _1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.Read();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1
+{
+    internal class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.Read();
+        }
+    }
+}
