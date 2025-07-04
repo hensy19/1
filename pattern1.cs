98d4f5717dde256b51923cafb2b29a02dd6dba5c
hensy19 <hensypatell19@outlook.com>
2025-07-04 22:41:27 +0530

    initial commit

---
+++ pattern1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rows = 5;
+        int columns = 5;
+
+        for (int i = 1; i <= rows; i++)
+        {
+            for (int j = 1; j <= columns; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(); // Move to next line
+        }
+    }
+}
